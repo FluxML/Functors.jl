95a0e1247efaa85aa6ab89209c3593b942dcd7d6
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-07-13 19:04:53 +0530

    fixes

--- docs/make.jl
+++ docs/make.jl
@@ -4,8 +4,8 @@ DocMeta.setdocmeta!(Functors, :DocTestSetup, :(using Functors); recursive = true
 
 makedocs(modules = [Functors],
          doctest = false, # VERSION == v"1.5",
-         sitename = "Functors",
-         pages = ["Home" => "index.md"]
+         sitename = "Functors.jl",
+         pages = ["Home" => "index.md"],
          format = Documenter.HTML(
              analytics = "UA-36890222-9",
              assets = ["assets/flux.css"],
