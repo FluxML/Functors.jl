184832b7041ef31b33d44023a03f5ab0f667aa0a
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2022-01-22 20:25:07 -0500

    functor some base types

--- src/base.jl
+++ src/base.jl
@@ -1,1 +1,24 @@
+
 @functor Base.RefValue
+
+functor(::Type{<:Base.ComposedFunction}, x) = (outer = x.outer, inner = x.inner), y -> Base.ComposedFunction(y.outer, y.inner)
+
+functor(::Type{<:PermutedDimsArray{T,N,perm}}, x) where {T,N,perm} = (parent = parent(x),), y -> PermutedDimsArray(only(y), perm)
+
+using Base.Iterators
+
+Functors.@functor Iterators.Drop (xs,)
+Functors.@functor Iterators.Enumerate
+# Functors.@functor Iterators.Flatten
+# Functors.@functor Iterators.ProductIterator
+# Functors.@functor Iterators.PartitionIterator
+Functors.@functor Iterators.Repeated
+Functors.@functor Iterators.Reverse
+Functors.@functor Iterators.Take (xs,)
+Functors.@functor Iterators.Zip
+
+using LinearAlgebra
+
+@functor Adjoint
+@functor Transpose
+@functor Diagonal
--- test/base.jl
+++ test/base.jl
@@ -1,8 +1,41 @@
-@testset "Base" begin
-    @testset "RefValue" begin
-        x = Ref(1)
-        p, re = Functors.functor(x)
-        @test p == (x = 1,)
-        @test re(p) isa Base.RefValue{Int}
-    end
+
+@testset "RefValue" begin
+    @test fmap(sqrt, Ref(16))[] == 4.0
+    @test fmap(sqrt, Ref(16)) isa Ref
+    @test fmapstructure(sqrt, Ref(16)) === (x = 4.0,)
+
+    x = Ref(1)
+    p, re = Functors.functor(x)
+    @test p == (x = 1,)
+    @test re(p) isa Base.RefValue{Int}
+end
+
+@testset "PermutedDimsArray" begin
+    @test fmapstructure(identity, PermutedDimsArray([1 2; 3 4], (2,1))) == (parent = [1 2; 3 4],)
+    @test fmap(exp, PermutedDimsArray([1 2; 3 4], (2,1))) isa PermutedDimsArray{Float64}
+end
+
+@testset "LinearAlgebra containers" begin
+    @test fmapstructure(identity, [1,2,3]') == (parent = [1, 2, 3],)
+    @test fmapstructure(identity, transpose([1,2,3])) == (parent = [1, 2, 3],)
+
+    CNT = Ref(0)
+    fv(x::Vector) = (CNT[]+=1; 10v)
+
+    v = [1,2,3]
+    nt = fmap(fv, (a=v, b=v', c=transpose(v), d=[1,2,3]'))
+
+    @test nt.a === adjoint(nt.b)  # does not break tie
+    @test nt.a === transpose(nt.c)
+
+    @test CNT[] == 2
+    @test nt.a == adjoint(nt.d)  # does not create a new tie
+    @test nt.a !== adjoint(nt.d)
+end
+
+@testset "Iterators" begin
+    # Iterators.repeated(([1,2,3], [4,5,6]), 4)
+
+    @test fmap(float, zip([1,2], [3,4])) isa Iterators.Zip
+    @test first(fmap(float, zip([1,2], [3,4]))) === (1.0, 3.0)
 end
