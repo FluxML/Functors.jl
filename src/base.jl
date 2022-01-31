c64d5faddbfc52eea602d79199bdcedd78309d9e
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2022-01-31 11:54:27 -0500

    functor Transpose, Adjoint, PermutedDimsArray

--- src/base.jl
+++ src/base.jl
@@ -1,1 +1,34 @@
+
 @functor Base.RefValue
+
+functor(::Type{<:Base.ComposedFunction}, x) = (outer = x.outer, inner = x.inner), y -> Base.ComposedFunction(y.outer, y.inner)
+
+using LinearAlgebra
+# The reason for these is to let W and W' be seen as tied weights in Flux models.
+# Can't treat ReshapedArray very well, as its type doesn't include enough details for reconstruction.
+
+functor(::Type{<:Adjoint}, x) = (parent = _adjoint(x),), y -> adjoint(only(y))
+
+_adjoint(x) = adjoint(x)  # _adjoint is the inverse, and also understands more types:
+_adjoint(x::NamedTuple{(:parent,)}) = x.parent  # "structural" gradient, and lazy broadcast used by Optimisers:
+_adjoint(bc::Broadcast.Broadcasted{S}) where S = Broadcast.Broadcasted{S}(_conjugate(bc.f, adjoint), _adjoint.(bc.args))
+
+functor(::Type{<:Transpose}, x) = (parent = _transpose(x),), y -> transpose(only(y))
+
+_transpose(x) = transpose(x)
+_transpose(x::NamedTuple{(:parent,)}) = x.parent
+_transpose(bc::Broadcast.Broadcasted{S}) where S = Broadcast.Broadcasted{S}(_conjugate(bc.f, transpose), _transpose.(bc.args))
+
+function functor(::Type{<:PermutedDimsArray{T,N,perm,iperm}}, x) where {T,N,perm,iperm}
+  (parent = _PermutedDimsArray(x, iperm),), y -> PermutedDimsArray(only(y), perm)
+end
+function functor(::Type{<:PermutedDimsArray{T,N,perm,iperm}}, x::PermutedDimsArray{Tx,N,perm,iperm}) where {T,Tx,N,perm,iperm}
+  (parent = parent(x),), y -> PermutedDimsArray(only(y), perm)  # most common case, avoid wrapping wrice.
+end
+
+_PermutedDimsArray(x, iperm) = PermutedDimsArray(x, iperm)
+_PermutedDimsArray(x::NamedTuple{(:parent,)}, iperm) = x.parent
+_PermutedDimsArray(bc::Broadcast.Broadcasted, iperm) = _PermutedDimsArray(Broadcast.materialize(bc), iperm)
+
+_conjugate(f::F, ::typeof(identity)) where F = f
+_conjugate(f::F, op::Union{typeof(transpose), typeof(adjoint)}) where F = (xs...,) -> op(f(op.(xs)...))
--- test/base.jl
+++ test/base.jl
@@ -1,8 +1,72 @@
-@testset "Base" begin
-    @testset "RefValue" begin
-        x = Ref(1)
-        p, re = Functors.functor(x)
-        @test p == (x = 1,)
-        @test re(p) isa Base.RefValue{Int}
-    end
+
+@testset "RefValue" begin
+  @test fmap(sqrt, Ref(16))[] == 4.0
+  @test fmap(sqrt, Ref(16)) isa Ref
+  @test fmapstructure(sqrt, Ref(16)) === (x = 4.0,)
+
+  x = Ref(1)
+  p, re = Functors.functor(x)
+  @test p == (x = 1,)
+  @test re(p) isa Base.RefValue{Int}
+end
+
+@static if VERSION >= v"1.6"
+  @testset "ComposedFunction" begin
+    f1 = Foo(1.1, 2.2)
+    f2 = Bar(3.3)
+    @test Functors.functor(f1 ∘ f2)[1] == (outer = f1, inner = f2)
+    @test Functors.functor(f1 ∘ f2)[2]((outer = f1, inner = f2)) == f1 ∘ f2
+    @test fmap(x -> x + 10, f1 ∘ f2) == Foo(11.1, 12.2) ∘ Bar(13.3)
+  end
+end
+
+@testset "PermutedDimsArray" begin
+  @test fmapstructure(identity, PermutedDimsArray([1 2; 3 4], (2,1))) == (parent = [1 2; 3 4],)
+  @test fmap(exp, PermutedDimsArray([1 2; 3 4], (2,1))) isa PermutedDimsArray{Float64}
+end
+
+@testset "LinearAlgebra containers" begin
+  @test fmapstructure(identity, [1,2,3]') == (parent = [1, 2, 3],)
+  @test fmapstructure(identity, transpose([1,2,3])) == (parent = [1, 2, 3],)
+
+  CNT = Ref(0)
+  fv(x::Vector) = (CNT[]+=1; 10v)
+
+  v = [1,2,3]
+  nt = fmap(fv, (a=v, b=v', c=transpose(v), d=[1,2,3]'))
+
+  @test nt.a === adjoint(nt.b)  # does not break tie
+  @test nt.a === transpose(nt.c)
+
+  @test CNT[] == 2
+  @test nt.a == adjoint(nt.d)  # does not create a new tie
+  @test nt.a !== adjoint(nt.d)
+
+  @test nt.b isa Adjoint
+  @test nt.c isa Transpose
+
+  x = [1,2,3]'
+  xs = fmapstructure(identity, x)  # check it digests this, e.g. structural gradient representation
+  @test Functors.functor(typeof(x), xs) == Functors.functor(x)  # (no real need for [2] types to match)
+
+  x = transpose([1 2; 3 4])
+  yt = transpose([5 6; 7 8])
+  ym = Matrix(yt)  # check it digests this, e.g. simplest Matrix gradient
+  @test Functors.functor(typeof(x), yt)[1].parent == Functors.functor(typeof(x), ym)[1].parent
+
+  ybc = Broadcast.broadcasted(+, ym, 9)  # check it digests this, as Optimisers.jl makes these
+  collect(ybc) isa Vector
+  zbc = Functors.functor(typeof(x), ybc)[1].parent
+  @test zbc .+ 0 == Functors.functor(typeof(x), ym .+ 9)[1].parent
+
+  # Similar checks for Adjoint. 
+  x = adjoint([1 2im 3; 4im 5 6im])
+  yt = adjoint([7im 8 9; 0 im 2])
+  ym = Matrix(yt)
+  @test Functors.functor(typeof(x), yt)[1].parent == Functors.functor(typeof(x), ym)[1].parent
+
+  ybc = Broadcast.broadcasted(+, ym, [11im, 12, im])
+  collect(ybc) isa Vector
+  zbc = Functors.functor(typeof(x), ybc)[1].parent
+  @test zbc .+ 0 == Functors.functor(typeof(x), ym .+ [11im, 12, im])[1].parent
 end
